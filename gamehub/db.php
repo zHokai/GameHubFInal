1fd884bee449ca33c1443e8ab2a708d7ffc6686b
Pedrak <luka.pedrak@edouardgand.fr>
2026-05-04 09:06:51 +0200

    Connexion a la base

---
+++ gamehub/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'gamehub';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+    exit;
+}
+?>
